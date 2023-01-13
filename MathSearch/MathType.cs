cccddf7089453627bf6c7cf452fbbaad562546c7
Toxic <markusbolsen@gmail.com>
2023-01-13 12:57:14 +0100

    Added methods for MathType.

--- MathSearch/MathType.cs
+++ MathSearch/MathType.cs
@@ -10,23 +10,27 @@ public enum MathType {
 }
 
 public static class UtilMathType {
+    public static bool Contains(this MathType type, MathExpression expression, MathSystem? context = null) {
+        return expression.DetermineType(context).IsSubTypeOf(type);
+    }
+
     public static bool IsSubTypeOf(this MathType sub, MathType super) {
-        return sub.Intersect(super) == sub;
+        return sub.IntersectWith(super) == sub;
     }
 
     public static bool IsSuperTypeOf(this MathType super, MathType sub) {
-        return sub.Intersect(super) == sub;
+        return sub.IntersectWith(super) == sub;
     }
 
-    public static MathType Intersect(this MathType type, MathType other) {
+    public static bool Overlaps(this MathType type, MathType other) {
+        return type.IntersectWith(other) == MathType.Nothing;
+    }
+
+    public static MathType IntersectWith(this MathType type, MathType other) {
         return type & other;
     }
 
-    public static MathType Union(this MathType type, MathType other) {
+    public static MathType UnionWith(this MathType type, MathType other) {
         return type | other;
-    }
-
-    public static bool Contains(this MathType type, MathExpression expression, MathSystem? context = null) {
-        return expression.DetermineType(context).IsSubTypeOf(type);
     }
 }
