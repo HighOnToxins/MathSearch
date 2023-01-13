04d825c65c6ab4591032d11122e84a87034dc100
Toxic <markusbolsen@gmail.com>
2023-01-13 16:19:40 +0100

    Added simplification from boolean type to set.

--- MathSearch/Expressions/Sets/TypeExpression.cs
+++ MathSearch/Expressions/Sets/TypeExpression.cs
@@ -1,5 +1,6 @@
 ﻿
 using MathSearch.Expression;
+using MathSearch.Expressions.Propersitions;
 
 namespace MathSearch.Expressions.Sets;
 
@@ -7,6 +8,15 @@ public sealed class TypeExpression: AtomExpression<MathType> {
     public override MathType Type => MathType.Set;
 
     public TypeExpression(MathType value) : base(value) {
+    }
+
+
+    public override MathExpression Simplify(MathSystem? context = null) {
+        if(Value == MathType.Boolean) {
+            return new SetExpression(new BooleanExpression(false), new BooleanExpression(true));
+        } else {
+            return base.Simplify(context);
+        }
     }
 
     public override MathExpression Clone() =>
